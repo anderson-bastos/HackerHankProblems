bf618f011eb9e94bb9bc084a5b20d279699db2de
Anderson Bastos <andersonmbastos@gmail.com>
2019-08-01 10:14:28 -0300

    Eletronics Shop - https://www.hackerrank.com/challenges/electronics-shop/problem

--- src/main/java/com/hackerhank/problems/implementations/ElectronicShop.java
+++ src/main/java/com/hackerhank/problems/implementations/ElectronicShop.java
@@ -13,10 +13,10 @@ public class ElectronicShop {
 		for (int i = 0; i < keyboardsLength; i++) {
 			for (int j = 0; j < drivesLength; j++) {
 				int sumProducts = keyboards[i] + drives[j];
-
+				
 				if(b == sumProducts)
 					return b;
-				if (b > sumProducts)
+				if (b > sumProducts && sumProducts > moneyEnough)
 					moneyEnough = sumProducts;
 			}
 		}
